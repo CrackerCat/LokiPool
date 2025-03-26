c3c1ecc3317aba4bc2a7178971abb3f4b25fc0f1
Le1a <le1a@qq.com>
2025-03-26 10:26:01 +0800

    Fix Bugs

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ const LOGO: &str = r#"
 ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝      ╚═════╝  ╚═════╝ ╚══════╝
 "#;
 
-const VERSION: &str = "v0.1.2";
+const VERSION: &str = "v0.1.3";
 
 #[tokio::main]
 async fn main() -> Result<()> {
